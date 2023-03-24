f988efdfaa30f60f8559a87d754d755903a787d1
NotThatRqd <67846317+NotThatRqd@users.noreply.github.com>
2023-03-23 17:10:30 -0700

    hi world!

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,13 +1,11 @@
 use mayo_lib::cpu::CPU;
 use mayo_lib::cpu::instructions::*;
-use mayo_lib::cpu::register::Register;
 use mayo_lib::create_memory::create_memory;
-use console::Term;
 use mayo_lib::devices::memory::Memory;
 use mayo_lib::devices::memory_mapper::MemoryMapper;
 use mayo_lib::devices::screen_device::ScreenDevice;
 
-const IP: u8  = 0;
+/*const IP: u8  = 0;
 const ACC: u8 = 1;
 const R1: u8  = 2;
 const R2: u8  = 3;
@@ -18,7 +16,8 @@ const R6: u8  = 7;
 const R7: u8  = 8;
 const R8: u8  = 9;
 const SP: u8 = 10;
-const FP: u8 = 11;
+const FP: u8 = 11;*/
+const R1: u8  = 2;
 
 fn main() {
     let mut memory = create_memory(256*256);
@@ -29,72 +28,24 @@ fn main() {
         i += 1;
     };
 
-    let subroutine_address = (0x30, 0x00);
+    let mut write_char = |char: char, pos: u8| {
+        add(MOV_LIT_REG);
+        add(0x00);
+        add(char as u8);
+        add(R1);
 
-    add(PSH_LIT);
-    add(0x33);
-    add(0x33);
-
-    add(PSH_LIT);
-    add(0x22);
-    add(0x22);
-
-    add(PSH_LIT);
-    add(0x11);
-    add(0x11);
-
-    add(MOV_LIT_REG);
-    add(0x12);
-    add(0x34);
-    add(R1);
-
-    add(MOV_LIT_REG);
-    add(0x56);
-    add(0x78);
-    add(R4);
-
-    add(PSH_LIT);
-    add(0x00);
-    add(0x00);
-
-    add(CAL_LIT);
-    add(subroutine_address.0);
-    add(subroutine_address.1);
-
-    add(PSH_LIT);
-    add(0x44);
-    add(0x44);
-
-    // Subroutine..
-    let mut i = 0x3000;
-    let mut add = |n: u8| {
-        memory[i] = n;
-        i += 1;
+        add(MOV_REG_MEM);
+        add(R1);
+        add(0x30);
+        add(pos);
     };
 
-    add(PSH_LIT);
-    add(0x01);
-    add(0x02);
+    for (i, char) in "Hi world!".chars().into_iter().enumerate() {
+        write_char(char, i as u8);
+    }
 
-    add(PSH_LIT);
-    add(0x03);
-    add(0x04);
+    add(HLT);
 
-    add(PSH_LIT);
-    add(0x05);
-    add(0x06);
-
-    add(MOV_LIT_REG);
-    add(0x07);
-    add(0x08);
-    add(R1);
-
-    add(MOV_LIT_REG);
-    add(0x09);
-    add(0x0A);
-    add(R8);
-
-    add(RET);
 
     let memory = Box::new(Memory::from_vec(memory));
 
@@ -106,18 +57,5 @@ fn main() {
 
     let mut cpu = CPU::new(mm);
 
-    loop {
-        cpu.step().unwrap();
-        cpu.debug();
-        println!();
-        cpu.view_memory_at(cpu.get_register(Register::Ip) as usize, 8)
-            .unwrap();
-        //                 the stack
-        cpu.view_memory_at(0xFFFF - 1 - 43, 44)
-            .unwrap();
-        println!();
-
-        // wait for some user input
-        Term::stdout().read_char().unwrap();
-    }
+    cpu.run();
 }
--- src/lib/devices/screen_device.rs
+++ src/lib/devices/screen_device.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+use console::Term;
 use crate::devices::device::Device;
 
 pub struct ScreenDevice;
@@ -17,11 +19,22 @@ impl Device for ScreenDevice {
         None
     }
 
-    fn write_at_u8(&mut self, offset: usize, num: u8) -> Result<(), ()> {
-        todo!()
+    fn write_at_u8(&mut self, _offset: usize, _num: u8) -> Result<(), ()> {
+        Err(())
     }
 
     fn write_at_u16(&mut self, offset: usize, num: u16) -> Result<(), ()> {
-        todo!()
+        let x = offset % 16;
+        let y = (offset as f64 / 16.0).floor() as usize;
+
+        // multiplied by 2 because it looks better
+        Term::stdout().move_cursor_to(x * 2, y)
+            .unwrap();
+
+        let character_value = (num & 0x00FF) as u8;
+
+        print!("{}", character_value as char);
+
+        Ok(())
     }
 }
